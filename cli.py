bd88a70e2aeb0aac604c56516711486d9d4dc682
Rafael <77760494+ralfsales@users.noreply.github.com>
2025-02-02 23:22:52 +0000

    Corrections on functions of reading and writing file

--- cli.py
+++ cli.py
@@ -9,12 +9,12 @@ while True:
 
     if user_action.startswith('add'):
         todo = user_action[4:]
-        todos = functions.get_todos('todos.txt')
+        todos = functions.get_todos()
         todos.append(todo + "\n")
-        functions.write_todos('todos.txt', todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
-        todos = functions.get_todos('todos.txt')
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -24,12 +24,12 @@ while True:
     elif user_action.startswith('edit'):
         try:
             number = int(user_action[5:])
-            todos = functions.get_todos('todos.txt')
+            todos = functions.get_todos()
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
             print("todo edited...", new_todo)
-            functions.write_todos('todos.txt', todos)
+            functions.write_todos(todos)
         except (ValueError, IndexError):
             print("Your command is not valid")
             continue
@@ -37,12 +37,12 @@ while True:
     elif user_action.startswith('remove'):
         try:
             number = int(user_action[6:])
-            todos = functions.get_todos('todos.txt')
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             print(f"{todo_to_remove} was removed from the list.")
             todos.pop(index)
-            functions.write_todos('todos.txt', todos)
+            functions.write_todos(todos)
         except (ValueError, IndexError):
             print("Your command is not valid")
             continue
